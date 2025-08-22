b73acafcdb836eeb31040bf078d7dba972ec2644
Laís Caneschi de Siqueira <lais.caneschi.siqueira@gmail.com>
2025-08-22 20:57:43 -0300

    condominios e novo

---
+++ src/app/condominios/novo/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Novo() {
+    return(
+        <div id="principal" className="min-h-screen flex items-center justify-center bg-black">
+        <h1 id="nome" className="text-2xl font-bold text-center">
+            novo condominio
+        </h1>
+    </div>
+    )
+}

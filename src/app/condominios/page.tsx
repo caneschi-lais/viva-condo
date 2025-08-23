5f207a6cabe3b58da89eed1be330315e80be6592
Laís Caneschi de Siqueira <lais.caneschi.siqueira@gmail.com>
2025-08-22 23:28:51 -0300

    pagina de condominios, moradores, usuarios e pasta service com api de condominios

--- src/app/condominios/page.tsx
+++ src/app/condominios/page.tsx
@@ -1,6 +1,24 @@
 "use client"
 
+import {useState, useEffect} from 'react';
+//useState permite criar uma variavel que toda vez que for alterada automaticamente o react atualiza a lista - no inicio
+//useEffect permite executar uma função quando o componente é montado ou atualizado - depois de carregar a tela é executado
+import { getCondominios, ICondominio} from '@/services/api-condominios';//@ - alias - representa a rota
+
 export default function ListaCondominios(){
+
+    const [condominios, setCondominios] = useState<ICondominio[]>([]); //variavel que recebe uma array de vetor vazio do tipo ICondominio
+
+    useEffect(() => {
+        const buscarCondominios = async () => {
+            const data = await getCondominios(); //chama a função que busca os condominios
+            console.log(data);
+            setCondominios(data); //atualiza o estado com os dados obtidos
+        }
+
+        buscarCondominios(); //chama a função para buscar os condominios
+    },[]); //array vazio significa que a função só será executada uma vez, quando o componente for montado
+
     return(
         <div className="p-6 max-w-full">
             <div className="mb-4 flex items-center justify-between gap-4">
@@ -22,11 +40,27 @@ export default function ListaCondominios(){
                         </tr>
                     </thead>
                     <tbody className="divide-y divide-gray-200 bg-white">
+                        {condominios.length === 0 ?(
+                            
                             <tr>
                                 <td className="px-4 py-3 text-sm text-gray-700" colSpan={7}>
                                     Nenhum condominio encontrado.
                                 </td>
                             </tr>
+                        ) : (
+                            condominios.map((condominio, index) => (
+                                <tr key={condominio.id_condominio} className='hover:bg-gray-50'>
+                                    <td className="px-4 py-3 whitespace-nowrap text-sm text-gray-500">{String(index + 1)}</td>
+                                    <td className="px-4 py-3 whitespace-nowrap text-sm text-gray-500">{condominio.nome_condominio}</td>
+                                    <td className="px-4 py-3 whitespace-nowrap text-sm text-gray-500">{condominio.endereco_condominio}</td>
+                                    <td className="px-4 py-3 whitespace-nowrap text-sm text-gray-500">{condominio.cidade_condominio}</td>
+                                    <td className="px-4 py-3 whitespace-nowrap text-sm text-gray-500">{condominio.uf_condominio}</td>
+                                    <td className="px-4 py-3 whitespace-nowrap text-sm text-gray-500">{condominio.tipo_condominio}</td>
+                                    <td className="px-4 py-3 whitespace-nowrap text-sm text-gray-500">
+                                    </td>
+                                </tr>
+                            ))
+                        )}
                     </tbody>
                 </table>
             </div>

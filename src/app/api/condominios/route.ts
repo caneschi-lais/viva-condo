a19c5150ec908324173310c1598713040d9a6c8f
Laís Caneschi de Siqueira <lais.caneschi.siqueira@gmail.com>
2025-09-19 22:59:26 -0300

    implementaçao supabase e api

---
+++ src/app/api/condominios/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import { getCondominios } from "@/services/condominio.service";
+
+//GET /api/condominios
+export async function GET(){
+    try {
+        const data = await getCondominios();
+
+        return NextResponse.json({
+            success: true,
+            count: data.length,
+            data,
+        }, {status: 200});
+
+    } catch (e: any) {
+        return NextResponse.json({
+            success: false,
+            error: e.message ?? "erro inesperado",
+        }, {status: 400});
+    }
+}
